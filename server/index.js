2349cb85e0ec07b2d79c1c95a4c7c2f0508793f9
Abhay yadav <yadavabhay8227.com>
2024-08-09 11:57:53 +0530

    "Added static file serving and React routing support to server"

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path';
+import url from 'url';
 
-//components
+// Import components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
 
+// Setup __dirname equivalent
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 dotenv.config();
 
 const app = express();
+
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(cors());
@@ -18,10 +24,10 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
+// Handle React routing, return index.html for all other routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-  
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 const PORT = 8000;
 const username = process.env.DB_USERNAME;
